5099a1738601180478fd9c7a4a02dc675281bcc9
Jesús Bellón <jbellon@zinio.com>
2019-05-28 09:42:41 +0200

    create projecte basic structure, classes and pods

---
+++ Verse/Verse/Network/Services/MovieService.swift
@@ -0,0 +1,9 @@
+//
+//  MovieService.swift
+//  Verse
+//
+//  Created by Jesus Bellon Quixal on 28/05/2019.
+//  Copyright © 2019 Jesus Bellon Quixal. All rights reserved.
+//
+
+import Foundation
---
+++ Verse/Verse/ViewModels/MovieProfileViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieProfileViewModel.swift
+//  Verse
+//
+//  Created by Jesus Bellon Quixal on 28/05/2019.
+//  Copyright © 2019 Jesus Bellon Quixal. All rights reserved.
+//
+
+import Foundation
